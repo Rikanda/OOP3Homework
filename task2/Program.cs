b566cd1324d60326a7d30e6e03fb18e6310b3eca
Natalya <soltk7@yandex.ru>
2021-10-17 20:55:35 +0300

    2е задание (строка с буквами в обратном порядке)

---
+++ task2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // тест метода
+            Console.WriteLine("Введите любой текст: ");
+            string test = Console.ReadLine();
+            Console.WriteLine(Invers(test));
+            
+            
+        }
+
+        public static string Invers(string text)
+        {
+            string invers = "";
+            string word;
+            for(int i=(text.Length-1); i>=0; i--)
+            {
+                word = text[i].ToString();
+                invers += word;
+
+            }
+            return invers;
+        }
+    }
+}
